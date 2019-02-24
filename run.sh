714259b3d004a9e172ad28822f89c49c11b769a9
Stanley Lee <stanley.s.lee@gmail.com>
2019-02-24 04:31:00 -0600

    Remove bash shell script not used for now

--- run.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-PGHOST='localhost' PGUSER='postgres' PGDATABASE='postgres' PGPASSWORD='passwd' PGPORT=5432 node server.js
-
--- run_debug.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-PGHOST='localhost' PGUSER='postgres' PGDATABASE='postgres' PGPASSWORD='passwd' PGPORT=5432 LOG_LEVEL='debug' node server.js
-
